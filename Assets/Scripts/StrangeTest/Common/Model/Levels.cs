ac25506bb2cd808a43a022ecc7b5135b44ed540c
SLHeureux <sebastien.lheureux@gmail.com>
2014-01-15 11:50:18 -0500

    * Stubbed next part of implementation. (Game module and PauseMenu module)

--- Assets/Scripts/StrangeTest/Common/Model/Levels.cs
+++ Assets/Scripts/StrangeTest/Common/Model/Levels.cs
@@ -2,7 +2,8 @@ namespace StrangeTest.Common
 {
 	public class Levels
 	{
-		public const string MAIN_MENU = "MainMenu";
+		public const string MAIN_MENU 	= "MainMenu";
+		public const string GAME 		= "Game";
 	}
 }
 
