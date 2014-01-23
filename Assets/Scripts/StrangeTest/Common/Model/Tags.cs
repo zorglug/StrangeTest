e2debf7e4d5fe199decb605e76e06fdd26d0232a
Sébastien L'Heureux <slheureux@hibernum.com>
2014-01-23 16:55:51 -0500

    * Added tags to all context roots.

--- Assets/Scripts/StrangeTest/Common/Model/Tags.cs
+++ Assets/Scripts/StrangeTest/Common/Model/Tags.cs
@@ -3,6 +3,9 @@ namespace StrangeTest.Common
 	public class Tags
 	{
 		public const string MAIN_CONTEXT = "MainContext";
+		public const string MAIN_MENU_CONTEXT = "MainMenuContext";
+		public const string GAME_CONTEXT = "GameContext";
+		public const string PAUSE_MENU_CONTEXT = "PauseMenuContext";
 	}
 }
 
